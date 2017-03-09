e4ad1b075d9d54bd8ae0df8099606c62c7c137b7
Fernan-Ramos <frs0012@alu.ubu.es>
2017-03-09 13:13:20 +0100

    Actualizado para usar angular $resource  #11

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,108 +1,29 @@
 'use strict';
 angular.module('testManagerApp')
-
-    .factory('menuFactory', function () {
+    .constant("baseURL", "http://localhost:3000/")
+    .service('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
         var menufac = {};
 
-        var cuestionarios = [
-
-            {
-                title: 'Cuanto sabes de paises?',
-                image: 'img/libro.jpg',
-                text: 'blabalbla',
-                _id: 0,
-                questions: [{
-                    _id: 0,
-                    title: 'paises Test',
-                    pregunta: 'blaa',
-                    image: 'img/mundo.jpg',
-                    r1: 'asi',
-                    r2: 'asao',
-                    r3: 'ja',
-                    r4: 'je',
-                    rcorrect: ['asi'],
-                    contestada: false
-                },
-                {
-                    _id: 1,
-                    title: 'paises Test',
-                    pregunta: 'blaa',
-                    image: 'img/programar.jpg',
-                    r1: 'asi',
-                    r2: 'asao',
-                    r3: 'ja',
-                    r4: 'je',
-                    rcorrect: ['asao'],
-                    contestada: false
-                },
-                {
-                    _id: 2,
-                    title: 'paises Test',
-                    pregunta: 'blaa',
-                    image: 'img/libro.jpg',
-                    r1: 'asi',
-                    r2: 'asao',
-                    r3: 'ja',
-                    r4: 'je',
-                    rcorrect: ['ja'],
-                    contestada: false
-
-                }
-                ]
-            }
-
-        ];
-
         menufac.getCuestionarios = function () {
-            return cuestionarios;
-        };
-
-        menufac.getCuestionario = function (index) {
-            return cuestionarios[index];
+            return $resource(baseURL + "cuestionarios/:id", null, { 'update': { method: 'PUT' } });
         };
 
         return menufac;
+    }])
 
-
-    })
-
-
-
-    .factory('testFactory', function () {
+    .factory('testFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
         var testfac = {};
 
-        var tests = [];
-        var stats = [];
-        var labels = [];
-        var series = [];
-        var data = [];
-
         testfac.getAnswers = function () {
-            return tests;
+            return $resource(baseURL + "tests", null, { 'update': { method: 'PUT' } });
         };
 
         testfac.getStats = function () {
-            return stats;
-        };
-
-        testfac.getStat = function (index) {
-            return stats[index];
-        };
-
-        testfac.getLabels = function () {
-            return labels;
-        };
-
-        testfac.getSeries = function () {
-            return series;
-        };
-
-        testfac.getData = function () {
-            return data;
+            return $resource(baseURL + "stats/:id", null, { 'update': { method: 'PUT' } });
         };
 
         return testfac;
 
-    });
+    }]);
