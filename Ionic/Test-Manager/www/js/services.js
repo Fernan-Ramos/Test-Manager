b2ee50547ba75590d97c8cf0cd833611a8b2aaf3
Fernan-Ramos <frs0012@alu.ubu.es>
2017-03-15 12:02:53 +0100

    Añadido services.js #14

---
+++ Ionic/Test-Manager/www/js/services.js
@@ -0,0 +1,13 @@
+'use strict';
+angular.module('testManager.services',['ngResource'])
+    .constant("baseURL", "http://localhost:3000/")
+    .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+        //var menufac = {};
+
+        menufac.getCuestionarios = function () {
+            return $resource(baseURL + "cuestionarios/:id", null, { 'update': { method: 'PUT' } });
+        };
+
+        //return menufac;
+    }]);
