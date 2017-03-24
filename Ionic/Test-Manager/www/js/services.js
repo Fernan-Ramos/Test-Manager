c5a69580af2ccc36d7c5eef4a00e42b73d0b6483
Fernan-Ramos <frs0012@alu.ubu.es>
2017-03-24 10:37:39 +0100

    Se elimina la funcion de cada factory #21 Ahora en cada factory solamente se devuelve el resource , no es necesario implementarlo en una función

--- Ionic/Test-Manager/www/js/services.js
+++ Ionic/Test-Manager/www/js/services.js
@@ -1,23 +1,15 @@
 'use strict';
 angular.module('testManager.services', ['ngResource', 'ngMaterial', 'chart.js'])
     .constant("baseURL", "http://localhost:3000/")
+    
     .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
-        var menufac = {};
+        return $resource(baseURL + "cuestionarios/:id", null, { 'update': { method: 'PUT' } });
 
-        menufac.getCuestionarios = function () {
-            return $resource(baseURL + "cuestionarios/:id", null, { 'update': { method: 'PUT' } });
-        };
-
-        return menufac;
     }])
 
     .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
-        var favFac = {};
 
-        favFac.getFavoritos = function () {
-            return $resource(baseURL + "favoritos/:id", null, { 'update': { method: 'PUT' } });
-        }
+        return $resource(baseURL + "favoritos/:id", null, { 'update': { method: 'PUT' } });
 
-        return favFac;
     }]);
