52b30f3b8fe917f8ab712a70053408d603da4a76
Fernan-Ramos <frs0012@alu.ubu.es>
2017-04-11 11:37:22 +0200

    Actualizada ruta favoriteRouter #27

--- rest-server-passport/models/favorites.js
+++ rest-server-passport/models/favorites.js
@@ -2,20 +2,37 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+
 var favoritoSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    text: {
+        type: String,
+    },
+    questions: [],
+    tests: [],
+    stats: [],
+}, {
+    timestamps: true
+});
+
+var favoritosSchema = new Schema({
     favoritos: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Cuestionario'
-        }],
+        favoritoSchema
+    ],
     postedBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }
 }, {
-        timestamps: true
-    });
+    timestamps: true
+});
 
-var Favorites = mongoose.model('Favorite', favoritoSchema);
+var Favorites = mongoose.model('Favorite', favoritosSchema);
 
 module.exports = Favorites;
--- rest-server-passport/routes/favoriteRouter.js
+++ rest-server-passport/routes/favoriteRouter.js
@@ -11,37 +11,42 @@ var Verify = require('./verify');
 
 favoriteRouter.route('/')
     .get(Verify.verifyOrdinaryUser, function (req, res, next) {
-        Favorites.find({'postedBy': req.decoded._doc._id})
+        Favorites.find({
+                'postedBy': req.decoded._id
+            })
             .populate('favoritos')
             .populate('postedBy')
             .exec(function (err, favorites) {
-                if (err) throw err;
+                if (err) return next(err);
+                console.log(favorites)
                 res.json(favorites);
             });
     })
 
     .post(Verify.verifyOrdinaryUser, function (req, res, next) {
-        Favorites.findOne({'postedBy': req.decoded._doc._id}, function (err, favorite) {
-            if (err) throw err;
+        Favorites.findOne({
+            'postedBy': req.decoded._id
+        }, function (err, favorite) {
+            if (err) return next(err);
             if (!favorite) {
                 Favorites.create(req.body, function (err, favorite) {
-                    if (err) throw err;
-                    console.log('Favorite created!');
-                    favorite.postedBy = req.decoded._doc._id;
-                    favorite.favoritos.push(req.body._id);
+                    if (err) return next(err);
+                    favorite.postedBy = req.decoded._id;
+                    favorite.favoritos.push(req.body);
                     favorite.save(function (err, favorite) {
-                        if (err) throw err;
+                        if (err) return next(err);
+
                         res.json(favorite);
                     });
                 });
             } else {
-                var cuestionario = req.body._id;
+                var cuestionario = req.body;
 
                 if (favorite.favoritos.indexOf(cuestionario) == -1) {
                     favorite.favoritos.push(cuestionario);
                 }
                 favorite.save(function (err, favorite) {
-                    if (err) throw err;
+                    if (err) return next(err);
                     res.json(favorite);
                 });
             }
@@ -49,8 +54,10 @@ favoriteRouter.route('/')
     })
 
     .delete(Verify.verifyOrdinaryUser, function (req, res, next) {
-        Favorites.remove({'postedBy': req.decoded._doc._id}, function (err, resp) {
-            if (err) throw err;
+        Favorites.remove({
+            'postedBy': req.decoded._id
+        }, function (err, resp) {
+            if (err) return next(err);
             res.json(resp);
         });
     });
@@ -58,9 +65,17 @@ favoriteRouter.route('/')
 favoriteRouter.route('/:cuestId')
 
     .delete(Verify.verifyOrdinaryUser, function (req, res, next) {
-        Favorites.findOneAndUpdate({'postedBy': req.decoded._doc._id}, {$pull: {favoritos: req.params.cuestId}}, function (err, favorite) {
-            if (err) throw err;
-            Favorites.findOne({'postedBy': req.decoded._doc._id}, function(err, favorite){
+        Favorites.findOneAndUpdate({
+            'postedBy': req.decoded._id
+        }, {
+            $pull: {
+                favoritos: req.params.cuestId
+            }
+        }, function (err, favorite) {
+            if (err) return next(err);
+            Favorites.findOne({
+                'postedBy': req.decoded._id
+            }, function (err, favorite) {
                 res.json(favorite);
             });
         });
