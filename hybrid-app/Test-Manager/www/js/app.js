99160423a1eefbee679e1c867b5b351cb37fea63
Fernan-Ramos <frs0012@alu.ubu.es>
2017-06-09 11:05:52 +0200

    Añadida licencia ficheros aplicación híbrida #45

--- hybrid-app/Test-Manager/www/js/app.js
+++ hybrid-app/Test-Manager/www/js/app.js
@@ -1,9 +1,20 @@
-// Ionic Starter App
+/**
+    Test-Manager
+    Copyright (c) 2016 - 2017 Fernán Ramos Sáiz
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * @author Fernán Ramos Saiz
  * @version 1.0
--- hybrid-app/Test-Manager/www/js/services.js
+++ hybrid-app/Test-Manager/www/js/services.js
@@ -1,4 +1,21 @@
 /**
+    Test-Manager
+    Copyright (c) 2016 - 2017 Fernán Ramos Sáiz
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * @author Fernán Ramos Saiz
  * @version 1.0
  * @description Fichero que almacena los servicios que permiten la comunicación con el servidor Back-End
