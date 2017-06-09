10d24d23986c3f43de2d66a3bba9a86f141965a4
Fernan-Ramos <frs0012@alu.ubu.es>
2017-06-09 10:49:41 +0200

    Añadida licencias a los ficheros de la aplicación web #45

--- web-app/app/scripts/services.js
+++ web-app/app/scripts/services.js
@@ -1,4 +1,22 @@
 /**
+    Test-Manager
+    Copyright (c) 2016 - 2017 Fernán Ramos Sáiz
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+/**
  * @author Fernán Ramos Saiz
  * @version 1.0
  * @description Fichero que almacena los servicios que permiten la comunicación con el servidor Back-End
@@ -261,7 +279,7 @@ angular.module('testManagerApp')
 
                 );
         };
-         /**
+        /**
          * @ngdoc method
          * @name isAuthenticated
          * @methodOf testManagerApp.factory:AuthFactory
@@ -271,7 +289,7 @@ angular.module('testManagerApp')
         authFac.isAuthenticated = function () {
             return isAuthenticated;
         };
-         /**
+        /**
          * @ngdoc method
          * @name getUsername
          * @methodOf testManagerApp.factory:AuthFactory
