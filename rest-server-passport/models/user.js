6f368aba0d366bfb93d0dac53471968ed26289e9
Fernan-Ramos <frs0012@alu.ubu.es>
2017-06-09 11:25:14 +0200

    Añadida licencia ficheros servidor #45

--- rest-server-passport/models/user.js
+++ rest-server-passport/models/user.js
@@ -1,3 +1,25 @@
+/**
+    Test-Manager
+    Copyright (c) 2016 - 2017 Fernán Ramos Sáiz
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @author Fernán Ramos Saiz
+ * @version 1.0
+ * @description Modelo mongoose de usuarios
+ */
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
@@ -19,7 +41,7 @@ var User = new Schema({
     }
 });
 
-User.methods.getName = function() {
+User.methods.getName = function () {
     return (this.firstname + ' ' + this.lastname);
 };
 
